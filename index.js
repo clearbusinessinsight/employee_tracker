95939b1eca7cf636fea78933f07404493e8c253b
clearbusinessinsight <choltz@cacbydesign.com>
2022-12-15 05:28:54 -0500

    recoding

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const inquirer = require('inquirer');
 const connection = require('./db/connection');
 const cTable = require('console.table');
 
-
 function newPrompt () {
   const promptChoices = [{
     type: 'list',
@@ -29,13 +28,13 @@ function newPrompt () {
   .then(response => {
     switch (response.action) {
       case 'View All Employees':
-        viewAll("EMPLOYEE");
+        viewEmployee();
         break;
       case 'View All Roles':
-        viewAll("ROLE");
+        viewRole();
         break;
       case 'View All Departments':
-        viewAll("DEPARTMENT");
+        viewDepartment();
         break;
       case 'ViewEmployees by Manager':
         viewEmployeeByManager();
@@ -71,11 +70,119 @@ function newPrompt () {
         connection.end();
     }
   })
-  .catch(err => {
-    console.error(err);
+  .catch(error => {
+    console.error(error);
   });
 }
   
+const viewEmployee = () => {
+  let query;
+  query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
+    R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
+    FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
+    LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+    LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id;`;
+  
+  connection.query(query, (error, res) => {
+    if (eror) throw error;
+    console.table(res);
+
+    newPrompt();
+  });
+};
+  
+const viewDepartment = () => {
+  let query;
+query = `SELECT * FROM DEPARTMENT`;
+  
+  connection.query(query, (error, res) => {
+    if (error) throw error;
+    console.table(res);
+
+    newPrompt();
+  });
+};
+
+const viewRole = () => {
+  let query;
+ query = `SELECT R.id AS id, title, salary, D.name AS department
+    FROM ROLE AS R LEFT JOIN DEPARTMENT AS D
+    ON R.department_id = D.id;`;
+  
+  connection.query(query, (error, res) => {
+    if (error) throw error;
+    console.table(res);
+
+    newPrompt();
+  });
+};
+
+const viewEmployeeByManager =  () => {
+  connection.query("SELECT * FROM EMPLOYEE", (error, employeeRes) => {
+    if (error) throw err;
+    const employeeChoice = [{
+      name: 'None',
+      value: 0
+    }];
+    employeeRes.forEach(({ first_name, last_name, id }) => {
+      employeeChoice.push({
+        name: first_name + " " + last_name,
+        value: id
+      });
+    });
+     
+    let questions = [
+      {
+        type: "list",
+        name: "manager_id",
+        choices: employeeChoice,
+         message: "Who do you want to update?"
+      },
+    ]
+  
+    inquier.prompt(questions)
+      .then(response => {
+        let manager_id, query;
+        if (response.manager_id) {
+          query = `SELECT E.id AS id,
+                          E.first_name AS first_name,
+                          E.last_name AS last_name, 
+                          R.title AS role,
+                          D.name AS department, 
+                          CONCAT(M.first_name, " ", M.last_name) AS manager
+                  FROM EMPLOYEE AS E 
+                          LEFT JOIN ROLE AS R ON E.role_id = R.id
+                          LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+                          LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id
+                  WHERE E.manager_id = ?;`;
+        } else {
+          manager_id = null;
+          query = `SELECT E.id AS id, 
+                          E.first_name AS first_name, 
+                          E.last_name AS last_name, 
+                          R.title AS role, 
+                          D.name AS department, 
+                          CONCAT(M.first_name, " ", M.last_name) AS manager
+                  FROM EMPLOYEE AS E 
+                          LEFT JOIN ROLE AS R ON E.role_id = R.id
+                          LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+                          LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id
+                  WHERE E.manager_id is null;`;
+        }
+        connection.query(query, [response.manager_id], (error, res) => {
+          if (error) throw error;
+          console.table(res);
+          newPrompt();
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      }); 
+  });
+}
+
+
+
 
 
 
